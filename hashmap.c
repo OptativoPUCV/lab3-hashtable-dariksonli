f9900df40f0d1e2ce858b95100e6abc23e2360da
Replit user <>
2023-09-07 22:16:42 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -139,7 +139,7 @@ Pair * firstMap(HashMap * map)
 
 Pair * nextMap(HashMap * map) 
 {
-  if(map == NULL) return NULL;
+  if(map == NULL || map->current == NULL) return NULL;
   long posicion = map->current;
   long i = posicion;
   while(true)
